dfe5fa4ef210c4945d9042f9877cea29beb914f7
Ronny Falk <ronny@rfxsoftware.com>
2017-10-27 12:51:19 -0400

    initial

---
+++ TestCD/LevelDB.h
@@ -0,0 +1,13 @@
+//
+//  LevelDB.h
+//  TestCD
+//
+//  Created by Ronny Falk on 2017-10-27.
+//  Copyright © 2017 RFx Software Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LevelDB : NSObject
+
+@end
---
+++ TestCD/LevelDB.mm
@@ -0,0 +1,22 @@
+//
+//  LevelDB.m
+//  TestCD
+//
+//  Created by Ronny Falk on 2017-10-27.
+//  Copyright © 2017 RFx Software Inc. All rights reserved.
+//
+
+#import "LevelDB.h"
+
+#import <leveldb/db.h>
+#import <leveldb/options.h>
+
+@implementation LevelDB
+
+- (void)test {
+    leveldb::DB *_db;
+    leveldb::ReadOptions _readOptions;
+    leveldb::WriteOptions _writeOptions;
+}
+
+@end
